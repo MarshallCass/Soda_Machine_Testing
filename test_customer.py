cb33b8020c1d698fad6e5589ee49a7e265c7de8f
AdamMeyers42 <adammeyers3551@gmail.com>
2021-10-13 11:02:53 -0700

    Added two tests for coins_add_to_wallet

--- test_customer.py
+++ test_customer.py
@@ -1,5 +1,6 @@
 from logging import RootLogger
 import unittest
+from coins import Coin
 from customer import Customer
 
 class TestGetWalletCoin(unittest.TestCase):
@@ -7,7 +8,7 @@ class TestGetWalletCoin(unittest.TestCase):
 
     def setUp(self):
         self.customer = Customer()
-
+        
     def test_can_return_quarter(self):
         """Pass in 'Quarter', method should return a Quarter instance"""
         returned_coin = self.customer.get_wallet_coin('Quarter')
@@ -32,6 +33,18 @@ class TestGetWalletCoin(unittest.TestCase):
         """Pass in a 'String' and return none"""
         return_coin = self.customer.get_wallet_coin('Running')
         self.assertIsNone(return_coin, None)
+
+    def test_3_coins_add_to_wallet(self):
+        """Pass in a list of 3 coins, len of customers wallets money list coincides"""
+        current_number_of_coins = len(self.customer.wallet.money)
+        self.customer.add_coins_to_wallet(['Quarter', 'Nickel', 'Dime'])
+        self.assertEqual(len(self.customer.wallet.money), current_number_of_coins + 3)
+
+    def test_0_coins_add_to_wallet(self):
+        """Pass in a list of 0 coins, len of customers wallets money list coincides"""
+        current_number_of_coins = len(self.customer.wallet.money)
+        self.customer.add_coins_to_wallet([])
+        self.assertEqual(len(self.customer.wallet.money), current_number_of_coins)
 
 if __name__ == '__main__':
     unittest.main()

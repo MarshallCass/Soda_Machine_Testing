f4ccf4604cc55e7314d55f8ae620c8b590cc2a41
Marshall C <MCass2101@gmail.com>
2021-10-14 13:19:23 -0400

    Validate main menu and try parse int done

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,22 +1,45 @@
 import unittest
-from customer import Customer
+import user_interface
+import os
 
-class TestGetWalletCoin(unittest.TestCase):
-    """Tests for Customer's get_wallet_coin method"""
+class TestValidateMainMenu(unittest.TestCase):
+    """Tests for Customer's User Interface Module"""
 
-    def setUp(self):
-        self.customer = Customer()
-
-    def test_can_return_quarter(self):
-        """Pass in 'Quarter', method should return a Quarter instance"""
-        returned_coin = self.customer.get_wallet_coin('Quarter')
-        self.assertEqual(returned_coin.value, .25)
+    def test_validate_main_menu_num_one(self):
+        """Pass in number 1 ensure (true,number) is returned"""
+        number_one = user_interface.validate_main_menu(1)
+        self.assertEqual(number_one, (True, 1))
     
-    def test_can_return_quarter(self):
-        """Pass in 'Dime', method should return a Dime instance"""
-        return_coin = self.customer.get_wallet_coin('Dime')
-        self.assertEqual(return_coin.value, .10)
+    def test_validate_main_menu_num_two(self):
+        """Pass in number 2 ensure (true,number) is returned"""
+        number_two = user_interface.validate_main_menu(2)
+        self.assertEqual(number_two, (True, 2))
 
+    def test_validate_main_menu_num_three(self):
+        """Pass in number 3 ensure (true,number) is returned"""
+        number_three = user_interface.validate_main_menu(3)
+        self.assertEqual(number_three, (True, 3))
+
+    def test_validate_main_menu_num_four(self):
+        """Pass in number 4 ensure (true,number) is returned"""
+        number_four = user_interface.validate_main_menu(4)
+        self.assertEqual(number_four, (True, 4))
+
+    def test_validate_main_menu_num_five(self):
+        """Pass in number 5 ensure (true,number) is returned"""
+        number_five = user_interface.validate_main_menu(5)
+        self.assertEqual(number_five, (False, None))
+
+class TestTryParseInt(unittest.TestCase):
+    """Test to turn string into integer"""
+    def test_try_parse_int(self):
+        """Test to see if number string changes to integer"""
+        parse_check = user_interface.try_parse_int("10")
+        self.assertEqual(parse_check, 10)
+    def test_try_parse_int_word(self):
+        """Test to see if entering anything orther than a number string will return 0"""
+        parse_check = user_interface.try_parse_int("hello")
+        self.assertEqual(parse_check, 0)
 
 if __name__ == '__main__':
     unittest.main()
